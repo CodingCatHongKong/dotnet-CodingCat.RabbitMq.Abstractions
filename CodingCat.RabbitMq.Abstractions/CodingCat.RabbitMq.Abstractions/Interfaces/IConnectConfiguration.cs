6361ec985868c66965bb59f82179812c0d3ff40c
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-01 20:56:20 +0800

    added support to connect rabbit mq with retry logic

---
+++ CodingCat.RabbitMq.Abstractions/CodingCat.RabbitMq.Abstractions/Interfaces/IConnectConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodingCat.RabbitMq.Abstractions.Interfaces
+{
+    public interface IConnectConfiguration
+    {
+        TimeSpan TimeoutPerTry { get; }
+        TimeSpan RetryInterval { get; }
+        uint RetryUpTo { get; }
+    }
+}
